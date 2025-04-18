44912a980a1019a668cc5787f239e1f364af6e2e
Mukhoma <marzuqmuhammad70@gmail.com>
2025-04-18 13:49:12 +0800

    Initial commit: React frontend skeleton with routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-4">Home Page</h1>
+      <p>Welcome to our application!</p>
+    </div>
+  );
+}
+
+export default Home; 
